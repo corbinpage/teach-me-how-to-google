dcd5d5c91c9d5bec8d59a8178dee7defc11df434
Corbin Page <corbin.page@gmail.com>
2014-01-28 17:57:07 -0500

    Search Functional

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,42 +1,35 @@
 class SearchController < ApplicationController
 	def new
-		@search = Search.new
 	end
 
-	def add
-		debugger
-		@session = Session.find(params[:id])
-		old_search = Search.find_by session_id: @session[:id]
-
-
-
-		@search = Search.new(
-			{
-		  	 	:session_id => old_search[:session_id],
-		  	 	:step => old_search[:step] + 1,
-		  	 	:search_num => old_search[:search_num] + 1,
-		  	 	:search_text => params[:searchText],
-		  	 	:status => "OK"
-	  	 	}
-	  	 )
-
-		@search.save
-		redirect_to @session
-	end
 
 	def create
 		@session = setup_initial_session
 		@search = Search.new()
 		@search = setup_initial_search(params[:searchText],@session[:id])
 		#logger.debug "|~~HERE~~~#{@search.format}~~~~~#{@session[:id]}"
-		
 
-		if @search.save
+		if perform_search(@search)
 			redirect_to @session
 		else
 			redirect('session/new')
 		end
 	end
+
+	def add
+		@session = Session.find(params[:id])
+		old_search = Search.where("session_id = ?", @session[:id]).last
+
+		@search = increment_search(params[:searchText], old_search)
+		perform_search(@search)
+	
+		if perform_search(@search)
+			redirect_to @session
+		else
+			redirect('session/new')
+		end
+	end
+
 
 	def show
 		@session = Session.find([:id])
@@ -49,6 +42,12 @@ class SearchController < ApplicationController
 	end
 
 	private
+		def perform_search(search)
+			# Insert Google Search here
+			search.save
+		end
+
+
 		def setup_initial_session
 			session = Session.new(
 				{
@@ -61,14 +60,25 @@ class SearchController < ApplicationController
 			return session
 		end
 
+		def increment_search(search_text=nil, old_search)
+			search = Search.new(
+				{
+					:step => old_search[:step].to_i + 1,
+					:search_num => old_search[:search_num] + 1,
+					:search_text => search_text,
+					:session_id => old_search[:session_id]
+				}
+			)
+			return search
+		end
+
 		def setup_initial_search(search_text=nil, session_id=nil)
 			search = Search.new(
 				{
 					:step => 1,
 					:search_num => 1,
 					:search_text => search_text,
-					:session_id => session_id,
-					:status => "okie doke"
+					:session_id => session_id
 				}
 			)
 			return search
